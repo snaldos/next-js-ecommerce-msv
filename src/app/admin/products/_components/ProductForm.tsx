fdddfdf789789a230bcd530ff6795d3727b5ed75
naldo <arnaldoflopes@gmail.com>
2024-07-26 14:29:23 +0100

    1:34 yt

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,22 +1,29 @@
 "use client";
 
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { formatCurrency } from "@/lib/formatters";
-import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { addProduct } from "@/app/admin/_actions/products";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+import { formatCurrency } from "@/lib/formatters";
+import { useState } from "react";
+import { addProduct, updateProduct } from "../../_actions/products";
 import { useFormState, useFormStatus } from "react-dom";
+import { Product } from "@prisma/client";
+import Image from "next/image";
 
-export function ProductForm() {
-  const [error, action] = useFormState(addProduct, {});
-  const [priceInCents, setPriceInCents] = useState<number>();
+export function ProductForm({ product }: { product?: Product | null }) {
+  const [error, action] = useFormState(
+    product == null ? addProduct : updateProduct.bind(null, product.id),
+    {},
+  );
+
+  const [priceInCents, setPriceInCents] = useState<number | undefined>(product?.priceInCents);
+
   return (
     <form action={action} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required />
+        <Input type="text" id="name" name="name" required defaultValue={product?.name || ""} />
         {error.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
@@ -29,23 +36,31 @@ export function ProductForm() {
           value={priceInCents}
           onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
         />
+        <div className="text-muted-foreground">{formatCurrency((priceInCents || 0) / 100)}</div>
+        {error.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
       </div>
-      <div className="text-muted-foreground">{formatCurrency((priceInCents || 0) / 100)}</div>
-      {error.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
-
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Textarea id="description" name="description" required />
+        <Textarea
+          id="description"
+          name="description"
+          required
+          defaultValue={product?.description}
+        />
         {error.description && <div className="text-destructive">{error.description}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="file">File</Label>
-        <Input type="file" id="file" name="file" required />
+        <Input type="file" id="file" name="file" required={product == null} />
+        {product != null && <div className="text-muted-foreground">{product.filePath}</div>}
         {error.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
         <Label htmlFor="image">Image</Label>
-        <Input type="file" id="image" name="image" required />
+        <Input type="file" id="image" name="image" required={product == null} />
+        {product != null && (
+          <Image src={product.imagePath} height="400" width="400" alt="Product Image" />
+        )}
         {error.image && <div className="text-destructive">{error.image}</div>}
       </div>
       <SubmitButton />
@@ -55,6 +70,7 @@ export function ProductForm() {
 
 function SubmitButton() {
   const { pending } = useFormStatus();
+
   return (
     <Button type="submit" disabled={pending}>
       {pending ? "Saving..." : "Save"}
